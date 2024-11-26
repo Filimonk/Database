6d2f529c37fb0d0c2c7e408fbf59cb66db350bdc
Filimonk <filimon.kosov.2005@mail.ru>
2024-11-26 23:51:09 +0300

    First part of homework

---
+++ test/test.cpp
@@ -0,0 +1,31 @@
+#ifndef DO_NOT_INCLUDE_SYS_LIBS
+
+// Area for system headers
+#include <bits/stdc++.h>
+
+#endif // DO_NOT_INCLUDE_SYS_LIBS
+
+#include "../include/database.h"
+
+using namespace std;
+
+#define int long long
+ 
+void solve() {
+    memdb::Database db;
+}
+
+int32_t main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+
+//freopen("input.txt", "r", stdin);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
